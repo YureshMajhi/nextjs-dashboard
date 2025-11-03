336404bef9dab61fa8c91af1dbe01ad131db0a38
Yuresh Majhi <yureshmee@gmail.com>
2025-11-03 16:54:04 +0545

    optimizing fonts and images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], weight: ["200", "400", "600", "800"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
